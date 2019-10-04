ceaca9515c59017828f0c7f3241ddb99bac1ba55
malhotrasahil <54015993+malhotrasahil@users.noreply.github.com>
2019-10-04 22:54:34 +0530

    pychrm_files

---
+++ pycharm/test.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+# a=np.array([False,True,False])
+# b=np.array([True, False, True])
+# c=a and b
+# print(c)
+
+
+# a=[False,False]
+# print(sum(a))
+#
+#
+# a=[['a','5'],['v','5']]
+# anp=np.array(a)
+# print(anp[:,1].astype(int))
+
+
+a=np.array([False,True] , )
+b=np.array([True,True])
+
+
+
+
+c= a & b
+print(c)
